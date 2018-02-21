a0621a7f3bde5402008c76dcaa405350bd7259b9
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2018-02-21 16:38:25 +0100

    Changed structure for providers

--- src/dashboard_conf/_1001_cedc_settings.py
+++ src/dashboard_conf/_1001_cedc_settings.py
@@ -4,25 +4,20 @@ AVAILABLE_THEMES.append(( 'cedc', pgettext_lazy("Cloud Veneto", "CED-C"), 'theme
 
 DEFAULT_THEME = 'cedc'
 
-HORIZON_CONFIG['identity_providers'].append(
-    {
-      'id' :          'infn_sso',
-      'context' :     '/dashboard-infn',
-      'path' :        '/dashboard-infn/auth/register/',
-      'description' : 'INFN AAI',
-      'logo' :        '/dashboard/static/dashboard/img/logoInfnAAI.png'
-    }
-)
+HORIZON_CONFIG['identity_providers']['infn_sso'] = {
+    'context' :     '/dashboard-infn',
+    'path' :        '/dashboard-infn/auth/register/',
+    'description' : 'INFN AAI',
+    'logo' :        '/dashboard/static/dashboard/img/logoInfnAAI.png'
+}
 
-HORIZON_CONFIG['identity_providers'].append(
-    { 
-      'id' :          'unipd_sso',
-      'context' :     '/dashboard-unipd',
-      'path' :        '/dashboard-unipd/auth/register/',
-      'description' : 'UniPD IdP',
-      'logo' :        '/dashboard/static/dashboard/img/logoUniPD.png'
-    }
-)
+HORIZON_CONFIG['identity_providers']['unipd_sso'] = {
+    'context' :     '/dashboard-unipd',
+    'path' :        '/dashboard-unipd/auth/register/',
+    'description' : 'UniPD IdP',
+    'logo' :        '/dashboard/static/dashboard/img/logoUniPD.png'
+}
+
 
 WEBSSO_IDP_MAPPING["infn_sso"] = ("infnaai", "mapped")
 WEBSSO_IDP_MAPPING["unipd_sso"] = ("unipdaai", "mapped")
